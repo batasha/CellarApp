4b51eeac68d0928a5550d1eb7647d68c97291e1b
Natasha Badillo <natasha.badillo@gmail.com>
2013-05-31 09:23:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CellarApp::Application.config.secret_token = '009f0f7b808dfd3c3ef470900dee35a5d3c68bcb22b6746764b8b287cd13dd26fa7ef6385431c878439a18f4a819b8615d87a6a149d32a95debe7cd551b326e8'
